ab7994a0d51952481716df96302069aca2266276
Andrew Rose <paro42796@gmail.com>
2017-05-22 17:19:09 -0700

    Adds Settings window

--- project/Settings.java
+++ project/Settings.java
@@ -18,6 +18,49 @@ public class Settings extends World
     public Settings()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(960, 540, 1); 
+        prepare();
     }
+    
+     private void prepare(){
+        // Top Toolbar
+        GreenfootImage topBar = new GreenfootImage("toolbar_topbar.png"); //the image that is drawed;
+        getBackground().drawImage(topBar, 0, 0);
+        
+        GreenfootImage settingsText = new GreenfootImage("text_settings.png");
+        getBackground().drawImage(settingsText, 438, 7);
+        
+        GreenfootImage textWelcome = new GreenfootImage("text_welcome.png"); //the image that is drawed;
+        getBackground().drawImage(textWelcome, 817, 10);
+        
+        //Toolbar Icons
+        ExitIcon exitIcon = new ExitIcon(new MainMenu());
+        addObject(exitIcon, 25, 20);
+        
+        // Settings Window
+        GreenfootImage settingsWindow = new GreenfootImage("window_settings.png");
+        getBackground().drawImage(settingsWindow, 250, 100);
+        
+        ConfirmButton confirmButton = new ConfirmButton();
+        addObject(confirmButton, 570, 400);
+        
+        CancelButton cancelButton = new CancelButton();
+        addObject(cancelButton, 390, 400);
+        
+        GreenfootImage soundBar = new GreenfootImage("image_sound.png");
+        getBackground().drawImage(soundBar, 489, 270);
+        
+        GreenfootImage fullscreen = new GreenfootImage("text_fullscreen.png");
+        getBackground().drawImage(fullscreen, 489, 220);
+        
+        GreenfootImage resolution = new GreenfootImage("text_resolution.png");
+        getBackground().drawImage(resolution, 489, 170);
+        
+        DropdownButton resDropdown = new DropdownButton();
+        addObject(resDropdown, 680, 183);
+        
+        DropdownButton fullscreenDropdown = new DropdownButton();
+        addObject(fullscreenDropdown, 553, 233);
+    }
+
 }
