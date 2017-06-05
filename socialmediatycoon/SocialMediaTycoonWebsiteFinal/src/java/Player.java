aeeb07ee5c180b57f00c632a7435ca70ce6d2e7d
bommysk <shubhamkahal@gmail.com>
2017-06-05 11:12:39 -0700

    removed extra files, fixed some bugs

---
+++ socialmediatycoon/SocialMediaTycoonWebsiteFinal/src/java/Player.java
@@ -0,0 +1,421 @@
+
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.ParseException;
+import java.util.ArrayList;
+import javax.annotation.ManagedBean;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.SessionScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
+import javax.inject.Named;
+import java.util.Date;
+import java.util.List;
+import java.util.TimeZone;
+import javax.el.ELContext;
+import javax.faces.bean.ManagedProperty;
+
+@Named(value = "player")
+@SessionScoped
+@ManagedBean
+public class Player implements Serializable {
+
+    @ManagedProperty(value = "#{login}")
+    private Login login;
+
+    public Login getLogin() {
+        return login;
+    }
+
+    public void setLogin(Login login) {
+        this.login = login;
+    }
+
+    private DBConnect dbConnect = new DBConnect();
+    private Integer id;
+    private String playerLogin;
+    private String playerPassword;
+    private String playerOldPassword;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String postalAddress;
+    private Date createdDate = new Date();
+    
+    public DBConnect getDbConnect() {
+        return dbConnect;
+    }
+
+    public void setDbConnect(DBConnect dbConnect) {
+        this.dbConnect = dbConnect;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    
+    public String getPlayerLogin() {
+        return playerLogin;
+    }
+
+    public void setPlayerLogin(String playerLogin) {
+        this.playerLogin = playerLogin;
+    }
+
+    public String getPlayerPassword() {
+        return playerPassword;
+    }
+
+    public void setPlayerPassword(String playerPassword) {
+        this.playerPassword = playerPassword;
+    }
+
+    public String getPlayerOldPassword() {
+        return playerOldPassword;
+    }
+
+    public void setPlayerOldPassword(String playerOldPassword) {
+        this.playerOldPassword = playerOldPassword;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPostalAddress() {
+        return postalAddress;
+    }
+
+    public void setPostalAddress(String postalAddress) {
+        this.postalAddress = postalAddress;
+    }
+
+    public String getPlayerLoginFromSession() {
+        ELContext elContext = FacesContext.getCurrentInstance().getELContext();
+        Login login = (Login) elContext.getELResolver().getValue(elContext, null, "login");
+    
+        return login.getPlayerLogin();
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date created_date) {
+        TimeZone.setDefault(TimeZone.getTimeZone("GMT"));
+        this.createdDate = created_date;
+    }
+    
+    public String createPlayer() throws SQLException, ParseException {
+        Connection con = dbConnect.getConnection();
+
+        if (con == null) {
+            throw new SQLException("Can't get database connection");
+        }
+        
+        con.setAutoCommit(false);
+
+        Statement statement = con.createStatement();
+
+        PreparedStatement preparedStatement = con.prepareStatement("insert into player(login, password, first_name, last_name, email, postal_address, created_date) values(?,?,?,?,?,?,?)");
+        preparedStatement.setString(1, playerLogin);
+        preparedStatement.setString(2, playerPassword);
+        preparedStatement.setString(3, firstName);
+        preparedStatement.setString(4, lastName);
+        preparedStatement.setString(5, email);
+        preparedStatement.setString(6, postalAddress);
+        preparedStatement.setDate(7, new java.sql.Date(createdDate.getTime()));
+        preparedStatement.executeUpdate();
+        
+        statement.close();
+        con.commit();
+        con.close();      
+        
+        clear();
+        
+        return "index";
+    }
+    
+    public String deletePlayer(String playerLogin) throws SQLException, ParseException {
+        Connection con = dbConnect.getConnection();
+
+        if (con == null) {
+            throw new SQLException("Can't get database connection");
+        }
+        
+        con.setAutoCommit(false);
+        
+        PreparedStatement preparedStatement = con.prepareStatement("delete from player where login = ?");
+        
+        preparedStatement.setString(1, playerLogin);
+        preparedStatement.executeUpdate();
+
+        con.commit();
+        con.close();
+                
+        return "viewPlayers";
+    }
+    
+    public void validateLogin(FacesContext context, UIComponent component, Object value)
+            throws ValidatorException, SQLException {
+        
+        Connection con = dbConnect.getConnection();
+        int count;
+        String submittedLogin = (String) value;
+
+        if (con == null) {
+            throw new SQLException("Can't get database connection");
+        }
+        
+        con.setAutoCommit(false);
+
+        PreparedStatement preparedStatement = con.prepareStatement("Select count(*) as count from player where login = ?");
+        preparedStatement.setString(1, submittedLogin);
+        
+        ResultSet result = preparedStatement.executeQuery();
+
+        result.next();
+        
+        count = result.getInt("count");
+        
+        if (count != 0) {
+            FacesMessage errorMessage = new FacesMessage("This login already exists, please pick another one.");
+            throw new ValidatorException(errorMessage);
+        }
+        
+        result.close();
+        con.close();
+    }
+    
+    public void validateLoginExistence(FacesContext context, UIComponent component, Object value)
+            throws ValidatorException, SQLException {
+        
+        Connection con = dbConnect.getConnection();
+        int count;
+        String submittedLogin = (String) value;
+        
+        if (con == null) {
+            throw new SQLException("Can't get database connection");
+        }
+        
+        con.setAutoCommit(false);
+
+        PreparedStatement preparedStatement = con.prepareStatement("select count(*) as count from player where login = ?");
+        preparedStatement.setString(1, submittedLogin);
+        
+        ResultSet result = preparedStatement.executeQuery();
+
+        result.next();
+        
+        count = result.getInt("count");
+        
+        result.close();
+        con.close();
+        
+        if (count == 0) {
+            FacesMessage errorMessage = new FacesMessage("This login does not exist.");
+            throw new ValidatorException(errorMessage);
+        }
+    }
+    
+    public List<Player> getPlayerList() throws SQLException {
+
+        Connection con = dbConnect.getConnection();
+
+        if (con == null) {
+            throw new SQLException("Can't get database connection");
+        }
+        
+        con.setAutoCommit(false);
+
+        PreparedStatement preparedStatement
+                = con.prepareStatement(
+                        "select login, first_name, last_name, email, postal_address, created_date from player order by first_name, last_name");
+
+        //get customer data from database
+        ResultSet result = preparedStatement.executeQuery();
+
+        List<Player> playerList = new ArrayList<>();
+
+        while (result.next()) {
+            Player player = new Player();
+
+            player.setPlayerLogin(result.getString("login"));
+            player.setFirstName(result.getString("first_name"));
+            player.setLastName(result.getString("last_name"));
+            player.setEmail(result.getString("email"));
+            player.setPostalAddress(result.getString("postal_address"));
+            player.setCreatedDate(result.getDate("created_date"));
+
+            //store all data into a List
+            playerList.add(player);
+        }
+        
+        result.close();
+        con.close();
+        
+        return playerList;
+    }
+    
+    public Integer getPlayerId(String playerLogin) throws SQLException {
+        Connection con = dbConnect.getConnection();
+
+        if (con == null) {
+            throw new SQLException("Can't get database connection");
+        }
+        
+        con.setAutoCommit(false);
+
+        PreparedStatement preparedStatement
+                = con.prepareStatement(
+                        "select id from player where login = ?");
+
+        preparedStatement.setString(1, playerLogin);
+        
+        //get customer data from database
+        ResultSet result = preparedStatement.executeQuery();
+        
+        result.next();
+        
+        return result.getInt("id");
+    }
+    
+    public String getPlayerLoginFromId(Integer id) throws SQLException {
+        Connection con = dbConnect.getConnection();
+
+        if (con == null) {
+            throw new SQLException("Can't get database connection");
+        }
+        
+        con.setAutoCommit(false);
+
+        PreparedStatement preparedStatement
+                = con.prepareStatement(
+                        "select login from player where id = ?");
+
+        preparedStatement.setInt(1, id);
+        
+        //get customer data from database
+        ResultSet result = preparedStatement.executeQuery();
+        
+        result.next();
+        
+        return result.getString("login");
+    }
+    
+    public void clear() {
+        setPlayerLogin(null);
+        setPlayerPassword(null);
+        setFirstName(null);
+        setLastName(null);
+        setEmail(null);
+        setPostalAddress(null);        
+    }
+    
+    public String changePassword() throws SQLException {
+       Connection con = dbConnect.getConnection();
+
+       if (con == null) {
+           throw new SQLException("Can't get database connection");
+       }
+       con.setAutoCommit(false);
+
+       Statement statement = con.createStatement();
+
+       PreparedStatement preparedStatement = con.prepareStatement("update player set password = ? where login = ?");
+       preparedStatement.setString(1, playerPassword);
+       preparedStatement.setString(2, Util.getPlayerLogin());
+       preparedStatement.executeUpdate();
+
+       statement.close();
+       con.commit();
+       con.close();
+       clear();
+
+       return "index";
+    }
+    
+    public void validateOldPassword(FacesContext context, UIComponent component, Object value)
+            throws ValidatorException, SQLException {
+        
+        String submittedPassword = (String) value;
+
+        Connection con = dbConnect.getConnection();
+        int count;
+
+        if (con == null) {
+            throw new SQLException("Can't get database connection");
+        }
+        
+        con.setAutoCommit(false);
+
+        PreparedStatement preparedStatement = con.prepareStatement("select count(*) as count from player where login = ? and password = ?");
+        preparedStatement.setString(1, Util.getPlayerLogin());
+        preparedStatement.setString(2, submittedPassword);
+        
+        ResultSet result = preparedStatement.executeQuery();
+        
+        result.next();
+        
+        count = result.getInt("count");
+        
+        if (count == 0) {
+            FacesMessage errorMessage = new FacesMessage("Incorrect old password.");
+            throw new ValidatorException(errorMessage);
+        }
+        
+        result.close();
+        con.close();
+    }
+    
+    public String setPlayerProfile() throws SQLException  {
+        Connection con = dbConnect.getConnection();
+
+        if (con == null) {
+            throw new SQLException("Can't get database connection");
+        }
+        
+        con.setAutoCommit(false);
+
+        PreparedStatement preparedStatement = con.prepareStatement("select first_name, last_name from player where login = ?");
+        preparedStatement.setString(1, Util.getPlayerLogin());
+        
+        ResultSet result = preparedStatement.executeQuery();
+        
+        result.next();
+        
+        this.firstName = result.getString("first_name");
+        this.lastName = result.getString("last_name");
+        
+        return "userProfile";
+    }
+}
---
+++ socialmediatycoon/SocialMediaTycoonWebsiteFinal/src/java/Tweet.java
@@ -0,0 +1,346 @@
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+import javax.annotation.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.inject.Named;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author shubhamkahal
+ */
+@Named(value = "tweet")
+@SessionScoped
+@ManagedBean
+public class Tweet {
+    private DBConnect dbConnect = new DBConnect();
+    private Integer id;
+    private String text;
+    private String destination = "Rome Tor Vergata";
+    private Player player = new Player();
+    private Integer score;
+    private Integer appropriate;
+    private Integer warning;
+    private Date createdDate = new Date();
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    
+    public DBConnect getDbConnect() {
+        return dbConnect;
+    }
+
+    public void setDbConnect(DBConnect dbConnect) {
+        this.dbConnect = dbConnect;
+    }
+    
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }        
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }        
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    public int getAppropriate() {
+        return appropriate;
+    }
+
+    public void setAppropriate(Integer appropriate) {
+        this.appropriate = appropriate;
+    }
+
+    public Integer getWarning() {
+        return warning;
+    }
+
+    public void setWarning(Integer warning) {
+        this.warning = warning;
+    }
+    
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+    
+    public String send() throws SQLException {        
+        Random randomGenerator = new Random();
+    
+        int randomScore = randomGenerator.nextInt(100);
+        int randomAppropriateness;
+        
+        if (randomScore < 10) {
+            randomAppropriateness = 0;
+        }
+        else {
+            randomAppropriateness = 1;
+        }
+                
+        Connection con = dbConnect.getConnection();
+
+        if (con == null) {
+            throw new SQLException("Can't get database connection");
+        }
+        
+        con.setAutoCommit(false);
+
+        Statement statement = con.createStatement();
+
+        PreparedStatement preparedStatement = con.prepareStatement("insert into tweet(text, player_id, destination_id, appropriate, score, created_date) "
+                + "                                                 values(?,(select id from player where login = ?),(select id from destination where name = ?),?,?,?)");
+        
+        preparedStatement.setString(1, this.text);
+        preparedStatement.setString(2, Util.getPlayerLogin());
+        preparedStatement.setString(3, this.destination);        
+        preparedStatement.setInt(4, randomAppropriateness);
+        preparedStatement.setInt(5, randomScore);
+        preparedStatement.setDate(6, new java.sql.Date(this.createdDate.getTime()));
+        preparedStatement.executeUpdate();
+        
+        statement.close();
+        con.commit();
+        con.close();      
+        
+        clear();
+        
+        return "index";
+    }
+    
+    public String saveDestination() throws SQLException {
+        Connection con = dbConnect.getConnection();
+
+        if (con == null) {
+            throw new SQLException("Can't get database connection");
+        }
+        
+        con.setAutoCommit(false);
+
+        Statement statement = con.createStatement();
+
+        PreparedStatement preparedStatement = con.prepareStatement("insert into destination(name) values(?) "
+                + "                                                 ON CONFLICT (name) DO NOTHING;");
+        
+        preparedStatement.setString(1, this.destination);
+        preparedStatement.executeUpdate();
+        
+        statement.close();
+        con.commit();
+        con.close();      
+        
+        return "sendTweet";
+    }
+    
+    public List<Tweet> getTweetList() throws SQLException {
+        Connection con = dbConnect.getConnection();
+        List<Tweet> tweetList = new ArrayList<>();
+        
+        if (con == null) {
+            throw new SQLException("Can't get database connection");
+        }
+
+        PreparedStatement preparedStatement
+                = con.prepareStatement("select text, login as player_login, destination.name as destination_name, tweet.score, tweet.warning, coalesce(reward.value, '') as reward_value, tweet.created_date from "
+                        + "tweet join player on tweet.player_id = player.id left join rewards on rewards.player_id = player.id "
+                        + " left join reward on rewards.reward_id = reward.id join destination on tweet.destination_id = destination.id where player.login = ?");
+        
+        preparedStatement.setString(1, Util.getPlayerLogin());
+        
+        ResultSet result = preparedStatement.executeQuery();        
+    
+        while (result.next()) {
+            Tweet tweet = new Tweet();                 
+            
+            tweet.setText("<span style='" + result.getString("reward_value") + "'> " + result.getString("text") + "</span>");
+            
+            tweet.player.setPlayerLogin(result.getString("player_login"));
+            
+            tweet.setDestination(result.getString("destination_name"));
+            
+            tweet.setScore(result.getInt("score"));
+            
+            tweet.setWarning(result.getInt("warning"));
+            
+            tweet.setCreatedDate(result.getDate("created_date"));          
+   
+            //store all data into a List
+            tweetList.add(tweet);
+        }
+        
+        result.close();
+        con.close();
+        
+        return tweetList;
+    }
+    
+    public List<Tweet> getComplaintsList() throws SQLException {
+        Connection con = dbConnect.getConnection();
+        List<Tweet> tweetList = new ArrayList<>();
+        
+        if (con == null) {
+            throw new SQLException("Can't get database connection");
+        }
+
+        PreparedStatement preparedStatement
+                = con.prepareStatement("select tweet.id, text, login as player_login, destination.name as destination_name, tweet.score, tweet.appropriate, coalesce(reward.value, '') as reward_value, tweet.created_date from "
+                        + "tweet join player on tweet.player_id = player.id left join rewards on rewards.player_id = player.id "
+                        + " left join reward on rewards.reward_id = reward.id join destination on tweet.destination_id = destination.id where tweet.appropriate = 0");               
+        
+        ResultSet result = preparedStatement.executeQuery();        
+    
+        while (result.next()) {
+            Tweet tweet = new Tweet();      
+            
+            tweet.setId(result.getInt("id"));
+            
+            tweet.setText("<span style='" + result.getString("reward_value") + "'> " + result.getString("text") + "</span>");
+            
+            tweet.player.setPlayerLogin(result.getString("player_login"));
+            
+            tweet.setDestination(result.getString("destination_name"));
+            
+            tweet.setScore(result.getInt("score"));
+            
+            tweet.setAppropriate(result.getInt("appropriate"));
+            
+            tweet.setCreatedDate(result.getDate("created_date"));          
+   
+            //store all data into a List
+            tweetList.add(tweet);
+        }
+        
+        result.close();
+        con.close();
+        
+        return tweetList;
+    }
+    
+    public List<Tweet> getLeaderList() throws SQLException {
+        Connection con = dbConnect.getConnection();
+        List<Tweet> leaderList = new ArrayList<>();
+        
+        if (con == null) {
+            throw new SQLException("Can't get database connection");
+        }
+
+        PreparedStatement preparedStatement
+                = con.prepareStatement("select login as player_login, sum(tweet.score) as sum_score from "
+                        + "tweet join player on tweet.player_id = player.id group by player.id order by sum_score desc");                
+        
+        ResultSet result = preparedStatement.executeQuery();        
+    
+        while (result.next()) {
+            Tweet tweet = new Tweet();                                         
+            
+            tweet.player.setPlayerLogin(result.getString("player_login"));
+            
+            tweet.setScore(result.getInt("sum_score"));                   
+   
+            //store all data into a List
+            leaderList.add(tweet);
+        }
+        
+        result.close();
+        con.close();
+        
+        return leaderList;
+    }
+    
+    public String sendWarning(int tweetId) throws SQLException {
+        Connection con = dbConnect.getConnection();
+
+        if (con == null) {
+            throw new SQLException("Can't get database connection");
+        }
+        
+        con.setAutoCommit(false);
+        
+        PreparedStatement preparedStatement = con.prepareStatement("update tweet set warning = 1 where id = ?");
+        
+        preparedStatement.setInt(1, tweetId);
+        preparedStatement.executeUpdate();
+
+        con.commit();
+        con.close();
+                
+        return "index";
+    }
+    
+    public String deleteTweet(int tweetId) throws SQLException {
+        Connection con = dbConnect.getConnection();
+
+        if (con == null) {
+            throw new SQLException("Can't get database connection");
+        }
+        
+        con.setAutoCommit(false);
+        
+        PreparedStatement preparedStatement = con.prepareStatement("delete from tweet where id = ?");
+        
+        preparedStatement.setInt(1, tweetId);
+        preparedStatement.executeUpdate();
+
+        con.commit();
+        con.close();
+                
+        return "index";
+    }
+    
+    public String checkWarning(Integer warning) {
+        if (warning != null && warning.equals(1)) {
+            return "text-danger";
+        }
+        
+        return "";
+    }
+    
+    public void clear() {
+        setText(null);
+        setDestination(null);
+    }
+
+}
