e3bf4618463dd6f9e6c54d8eaea4b2ccb6b8320c
bommysk <shubhamkahal@gmail.com>
2017-06-08 12:09:32 -0700

    removed smells

--- socialmediatycoon/SocialMediaTycoonWebsiteFinal/src/java/test/TestTweetSave.java
+++ socialmediatycoon/SocialMediaTycoonWebsiteFinal/src/java/test/TestTweetSave.java
@@ -2,29 +2,31 @@ package test;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
-import Tweet;
 /*
  * Test class written by Michael Murdock
  */
 public class TestTweet {
+	private static final String TEXTWORKS = "text works";
+	private static final String DESTINATIONWORKS = "destination works";
+
 	@Test
 	public void testInfo(){
 		Tweet testTweet = new Tweet();
 		testTweet.setId(5);
-		testTweet.setText("text works");
-		testTweet.setDestination("destination works");
+		testTweet.setText(TEXTWORKS);
+		testTweet.setDestination(DESTINATIONWORKS);
 		testTweet.setScore(6);
 		
 		assertEquals(5, testTweet.getId());
-		assertEquals("text works", testTweet.getText());
-		assertEquals("destination works", testTweet.getDestination());
+		assertEquals(TEXTWORKS, testTweet.getText());
+		assertEquals(DESTINATIONWORKS, testTweet.getDestination());
 		assertEquals(6, testTweet.getScore());
 	}
 	
 	public void testClear() {
 		Tweet testTweet = new Tweet();
-		testTweet.setTest("text works");
-		testTweet.setDestination("destination works");
+		testTweet.setTest(TEXTWORKS);
+		testTweet.setDestination(DESTINATIONWORKS);
 		testTweet.clear();
 		
 		assertNull(testTweet.getText());
